b0f0f61119458167eac948435f8a677ecfc1f010
Rodrigo A. Hidalgo <92281098+RodHid@users.noreply.github.com>
2021-10-30 08:49:23 -0600

    Update database.php

--- core/database.php
+++ core/database.php
@@ -8,8 +8,8 @@
         public function __construct(){
             $this->host = 'localhost';
             $this->user = 'root';
-            $this->pass = '1523';
-            $this->db = 'mvc';
+            $this->pass = '';
+            $this->db = 'exFinal';
         }
 
         public function conectar(){
